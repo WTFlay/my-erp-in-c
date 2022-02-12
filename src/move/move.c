76ead01b284e24e810ebc0d42c1eeb3f1bec3789
Flavien Schriever <flavien@schriever.dev>
2022-02-12 18:06:42 +0100

    Apply snake_case naming

--- src/move/move.c
+++ src/move/move.c
@@ -4,36 +4,36 @@ Move * move_new(Customer *customer);
 
 Move * move_create_from_invoice(Invoice *invoice) {
   Move *move = move_new(invoice->customer);
-  MoveLine *customerMoveLine = move_line_new(move->customer->account, 0, 0);
-  MoveLine *lastMoveLineInList = customerMoveLine;
+  MoveLine *customer_move_line = move_line_new(move->customer->account, 0, 0);
+  MoveLine *last_move_line_in_list = customer_move_line;
   for (int i = 0; i < invoice->invoice_line_list->len; i++) {
-    InvoiceLine *invoiceLine = invoice->invoice_line_list->list[i];
+    InvoiceLine *invoice_line = invoice->invoice_line_list->list[i];
 
-    Account *productAccount = invoiceLine->product->account;
-    MoveLine *productMoveLine = move_line_find_by_account(customerMoveLine, productAccount);
-    double priceWithoutTax = invoice_line_compute_total_price_without_tax(invoiceLine);
-    if (productMoveLine == NULL) {
-      productMoveLine = move_line_new(productAccount, 0, priceWithoutTax);
-      lastMoveLineInList->next_move_line = productMoveLine;
-      lastMoveLineInList = productMoveLine;
+    Account *product_account = invoice_line->product->account;
+    MoveLine *product_move_line = move_line_find_by_account(customer_move_line, product_account);
+    double price_without_tax = invoice_line_compute_total_price_without_tax(invoice_line);
+    if (product_move_line == NULL) {
+      product_move_line = move_line_new(product_account, 0, price_without_tax);
+      last_move_line_in_list->next_move_line = product_move_line;
+      last_move_line_in_list = product_move_line;
     } else {
-      productMoveLine->credit += priceWithoutTax;
+      product_move_line->credit += price_without_tax;
     }
 
-    Account *productTaxAccount = invoiceLine->product->tax->account;
-    MoveLine  *taxMoveLine = move_line_find_by_account(customerMoveLine, productTaxAccount);
-    double taxPrice = invoice_line_compute_total_tax(invoiceLine);
-    if (taxMoveLine == NULL) {
-      taxMoveLine = move_line_new(productTaxAccount, 0, taxPrice);
-      lastMoveLineInList->next_move_line = taxMoveLine;
-      lastMoveLineInList = taxMoveLine;
+    Account *product_tax_account = invoice_line->product->tax->account;
+    MoveLine  *tax_move_line = move_line_find_by_account(customer_move_line, product_tax_account);
+    double tax_price = invoice_line_compute_total_tax(invoice_line);
+    if (tax_move_line == NULL) {
+      tax_move_line = move_line_new(product_tax_account, 0, tax_price);
+      last_move_line_in_list->next_move_line = tax_move_line;
+      last_move_line_in_list = tax_move_line;
     } else {
-      taxMoveLine->credit += taxPrice;
+      tax_move_line->credit += tax_price;
     }
 
-    customerMoveLine->debit += priceWithoutTax + taxPrice;
+    customer_move_line->debit += price_without_tax + tax_price;
   }
-  move->move_line_list = customerMoveLine;
+  move->move_line_list = customer_move_line;
   return move;
 }
 
